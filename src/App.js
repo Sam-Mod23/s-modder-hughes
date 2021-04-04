75d9a231336ea1284b5e176f4d9c87ad982cc8f9
Sam Modder-Hughes <sjmodderhug@gmail.com>
2021-04-04 20:23:10 +0100

    basic colour and arrangement setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import AboutMe from './text/About-me.jsx';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'></header>
+      <header className='App-header'>Sam Modder-Hughes</header>
+      <body className='Body'>
+        <AboutMe className='aboutMe' />
+        <section className='My-Work'>My work goes here</section>
+      </body>
     </div>
   );
 }
---
+++ src/text/About-me.jsx
@@ -0,0 +1,36 @@
+const AboutMe = () => {
+  return (
+    <section>
+      <h2 className='aboutMe'>About me</h2>
+      <body className='aboutMe'>
+        Hi, I’m Sam! I’m a Physics graduate, turned banking contractor, turned
+        software developer currently working through a 12 week bootcamp at
+        Northcoders! After realising that my life as a Physicist was not as
+        Brian Cox would have you believe, think less awe-inspiring mountain
+        ranges and more darkened room with a book of equations, upon graduation
+        I stumbled into the banking sector. After multiple contract extensions,
+        giving me 7.5 years of experience with a major financial institution, I
+        turned 30, the contract ended and I decided it was time to expand my
+        skill-set. I’ve always been interested in tech and development, but only
+        from a user perspective. What I’ve loved about this course is how
+        accessible it has made the once elusive areas of tech. The fact that in
+        week 4 we developed a (admittedly rudimentary) Pokemon game completely
+        opened up my perspective on the whole industry. FYI - my Pokedex only
+        included the first generation, I’m a purist. In case it wasn’t already
+        apparent, and completely against stereotype…I’m a training software
+        developer who’s essentially just a big nerd - Star Wars, Marvel, Lego,
+        comic-con, give me it all! I’m fascinated by the games industry, and
+        love the way story telling is evoked through that medium. If only I had
+        more time to play them! I also couldn’t imagine my life without music.
+        I’ve basically been to a music festival every year of my life since I
+        was 17, and I barely have a moment without something playing in the
+        background. I had planned for a conventions/gig tour of the USA last
+        year, which I hope to rearrange soon! Most of all these past few months
+        of preparing for and taking part in the bootcamp have just made me
+        realise how much I have missed learning something new. I can’t wait for
+        this to continue.
+      </body>
+    </section>
+  );
+};
+export default AboutMe;
