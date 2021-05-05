25fa16afae4d9217b9a0cd9b243172713512484a
Sam Modder-Hughes <sjmodderhug@gmail.com>
2021-05-05 12:23:46 +0100

    restart process

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import './App.css';
-import AboutMe from './text/About-me.jsx';
-import Header from './components/Header';
-import Projects from './components/Projects';
-import Logo from './images/Logo.png';
-import Autumn from './images/Autumn.jpg';
+import "./App.css";
+import AboutMe from "./text/About-me.jsx";
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+import Logo from "./images/Logo.png";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
-      {/* <section style={{ 'background-color': '#374550' }}></section> */}
-      <section className='break'></section>
-      <body className='Body'>
-        <img className='photo' alt='Sam M-H' src={Logo}></img>
-        <AboutMe className='aboutMe' />
-        {/* <img className='background' alt='autumn' src={Autumn}></img> */}
-        <Projects />
-      </body>
+      <section className="photo-quicklinks">
+        <img className="photo" alt="Sam M-H" src={Logo}></img>
+        <div>
+          <a href="" className="work" style={{ margin: "1rem" }}>
+            A look at my work...
+          </a>
+          <a className="soundtrack" style={{ margin: "1rem" }}>
+            Something different...
+          </a>
+        </div>
+      </section>
+      <AboutMe className="aboutMe" />
+
+      <Projects />
     </div>
   );
 }
