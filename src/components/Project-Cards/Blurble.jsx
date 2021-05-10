5f47b7595ab84a0231616be74b52f5fd527682a7
Sam Modder-Hughes <sjmodderhug@gmail.com>
2021-05-10 17:06:53 +0100

    change to class component for blurble demo pop-up

--- src/components/Project-Cards/Blurble.jsx
+++ src/components/Project-Cards/Blurble.jsx
@@ -1,48 +1,63 @@
 import blurble from "../../images/blurble.jpg";
 
-const Blurble = () => {
-  return (
-    <div
-      href="#"
-      className="project-card"
-      style={{ backgroundImage: `url(${blurble})` }}
-    >
-      <h2 className="project-card-header">Blurble</h2>
-      <p className="project-card-text">
-        A book-club app for friends. Built using MongoDB and React Native.
-        Having complete independence to work on this project, our team decided
-        which technologies, platforms and packages to use. I worked primarily on
-        the backend, exploring a completely unfamiliar database package in
-        MongoDB and implementing it from the ground up.
-        <p>Javascript ✦ MongoDB ✦ Mongoose ✦ Express ✦ Jest ✦ Supertest</p>
-        <p>
-          <a
-            target="_blank"
-            href="https://github.com/Sam-Mod23/Blurble-Backend"
-            className="project-links"
-          >
-            Backend repo
-          </a>
-          <span>
-            {" | "}
+import React, { Component } from "react";
+
+export default class Blurble extends Component {
+  state = {
+    demoPop: false
+  };
+
+  togglePop = () => {
+    this.setState({
+      demoPop: !this.state.demoPop
+    });
+  };
+
+  render() {
+    return (
+      <div
+        rel="noreferrer"
+        href="#"
+        className="project-card"
+        style={{ backgroundImage: `url(${blurble})` }}
+      >
+        <h2 className="project-card-header">Blurble</h2>
+        <p className="project-card-text">
+          A book-club app for friends. Built using MongoDB and React Native.
+          Having complete independence to work on this project, our team decided
+          which technologies, platforms and packages to use. I worked primarily
+          on the backend, exploring a completely unfamiliar database package in
+          MongoDB and implementing it from the ground up.
+          <p>Javascript ✦ MongoDB ✦ Mongoose ✦ Express ✦ Jest ✦ Supertest</p>
+          <p>
             <a
+              rel="noreferrer"
               target="_blank"
-              href="https://blurble-project.herokuapp.com/api"
+              href="https://github.com/Sam-Mod23/Blurble-Backend"
               className="project-links"
             >
-              Heroku app
+              Backend repo
             </a>
-          </span>
-          <span>
-            {" | "}
-            <a target="_blank" href="#" className="project-links">
-              Demo and full presentation incoming!
-            </a>
-          </span>
+            <span>
+              {" | "}
+              <a
+                rel="noreferrer"
+                target="_blank"
+                href="https://blurble-project.herokuapp.com/api"
+                className="project-links"
+              >
+                Heroku app
+              </a>
+            </span>
+            <span>
+              {" | "}
+              <button onClick={this.togglePop} className="demo-button">
+                Demo
+              </button>
+            </span>
+          </p>
         </p>
-      </p>
-    </div>
-  );
-};
-
-export default Blurble;
+      </div>
+    );
+  }
+}
